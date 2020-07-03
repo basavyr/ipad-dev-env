9ba9bafdc4ca961ba14bd9b7af7bb1496198439e
Robert <robert.poenaru@protonmail.ch>
2020-07-03 19:16:34 +0000

    adds fix for eigenvalues test

--- Python/eigen/eigenvals.py
+++ Python/eigen/eigenvals.py
@@ -63,7 +63,8 @@ def IdentityMatrix(n):
         m.append(line)
     return m
 
-id3=IdentityMatrix(3)
+
+id3 = IdentityMatrix(3)
 
 matrix_vec = [[1, 2, 3], [0, 1, 1], [2, 2, 2]]
 
@@ -75,28 +76,37 @@ for value in values:
     # print(f'Its corresponding vector is: v[{count}]={vectors[:,count-1]}')
     count = count+1
 
-def CreateLambdaMatrix(n,lambdas):
-    if(n!=len(lambdas)):
+
+def CreateLambdaMatrix(n, lambdas):
+    if(n != len(lambdas)):
         return 'Error: the matrix is invalid'
-    count=0
-    m=[]
+    count = 0
+    m = []
     for column in range(n):
-        line=[]
+        line = []
         for row in range(n):
-            if(row==column):
+            if(row == column):
                 line.append(lambdas[count])
-                count=count+1
+                count = count+1
             else:
                 line.append(0)
         m.append(line)
     return m
 
-lambdas=CreateLambdaMatrix(3,values)
-lhs=np.matmul(lambdas,np.transpose(vectors))
-print(lhs)
-rhs=np.matmul(matrix_vec,np.transpose(vectors))
-print(rhs)
 
+lambdas = CreateLambdaMatrix(3, values)
+lhs = np.matmul(lambdas, np.transpose(vectors))
+# print(lhs)
+rhs = np.matmul(matrix_vec, np.transpose(vectors))
+# print(rhs)
+
+x0=vectors[:,0]
+
+product=np.matmul(matrix_vec,x0)
+print(product)
+
+rhs=values[0]*vectors[:,0]
+print(rhs)
 # This is implemented using the _geev LAPACK routines which compute the eigenvalues and eigenvectors of general square arrays.
 
 # The number w is an eigenvalue of a if there exists a vector v such that a @ v = w \* v. Thus, the arrays a, w, and v satisfy the equations a @ v[:,i] = w[i] \* v[:,i] for :math:i \in \{0,...,M-1\}.
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ipad-dev-env
 A project that contains different implementations in C++, Python, Mathematica and JavaScript. Project is developed from my personal iPad Pro.
+
+
+## Pushing updates to GitHub
+
+In order to push any updates to this repo from the iPad's VS Code app (that uses dedicated server to get the `code-server` running), without intorducing the username and password everytime, the following command must be use in the terminal.
+
+```
+git remote set-url origin git@github.com:basavyr/ipad-dev-env.git
+```
