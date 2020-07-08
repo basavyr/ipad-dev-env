0abcd1694b9ab4ef7740e9928643194aba382cf7
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-07-08 14:27:26 +0300

    adds symbolic matrix definition

--- Python/py_eigen_matrix/vectors.py
+++ Python/py_eigen_matrix/vectors.py
@@ -1,3 +1,29 @@
-test_v=[1,2,3]
+import numpy as np
+from numpy import random as rd
+from numpy import linalg as LA
 
-test_arr=[[1, 1, 1 ] ,[2, 2, 2 ]]
+m0=[[1, 1, 1 ] ,[2, 2, 2 ]]
+
+def Matrix(n):
+    m=[]
+    for id in range(n):
+        line=[]
+        for line_id in range(n):
+            line.append(id+1)
+        m.append(line)
+    return m
+
+def SymbMatrix(n):
+    m=[]
+    for id in range(n):
+        line=[]
+        for line_id in range(n):
+            element='v'+str(id+1)+str(line_id+1)
+            line.append(element)
+        m.append(line)
+    return m
+
+m1=Matrix(3)
+m1_symb=SymbMatrix(3)
+for id in range(len(m1_symb)):
+    print(m1_symb[id])
